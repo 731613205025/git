8f2ee2f1b3e65b6028a28e1d4cc0c3c1580a6b89
731613205025 <maniharish95@gmail.com>
2016-07-18 13:33:29 +0530

    Add files via upload

---
+++ Day.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Day {
+
+  
+    public static void main(String[] args) {
+       char a;
+        String s1;
+        Scanner sc=new Scanner(System.in);
+        s1=sc.nextLine();
+        if(s1.equalsIgnoreCase("sunday")){
+            System.out.println("false");
+        }else{
+            System.out.println("true");
+        }
+    }
+    }
+
